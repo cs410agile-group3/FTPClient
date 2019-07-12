a933ccc75ea320d25d12eb08c87a2555916b2b9e
Michelle Duer <michelle.duer@gmail.com>
2019-07-11 19:44:14 -0700

    add comments, make public for testing, move usage details to parseArgs
    add tests for parsing arguments

--- FtpClient/FtpCli/Program.cs
+++ FtpClient/FtpCli/Program.cs
@@ -2,11 +2,11 @@
 
 namespace FtpCli
 {
-    class Program
+    public class Program
     {
-        string server;
-        string user;
-        string[] commands;
+        public string server;
+        public string user;
+        public string[] commands;
 
         public Program() {
             server = "";
@@ -16,24 +16,35 @@ namespace FtpCli
 
         static void Main(string[] args)
         {
-            string usage = "servername username [-cmd, args, ...]";
-            while (args.Length < 2) {
-                Console.WriteLine($"\nUSAGE:\n{usage}\n\nUSAGE (to start program and server):\ndotnet run {usage}\n\nPlease enter a valid command:");
-                args = Console.ReadLine().Split();
-            }
-
             Program FTP = new Program();
 
             FTP.parseArgs(args);
         }
 
+
+        // parses command line arguments
         public void parseArgs(string[] args) {
+
+            // if an incorect number of parameters were passed
+            // print usage details and prompt user again
+            string usage = "servername username [cmd, args, ...]";
+            while (args.Length < 2) {
+                Console.WriteLine($"\nUSAGE:\n{usage}\n\n");
+                Console.WriteLine($"USAGE (to start program and server):\ndotnet run {usage}\n\n");
+                Console.WriteLine("Please enter valid arguments:");
+                args = Console.ReadLine().Split();
+            }
+
+            // set server and user values
             this.server = args[0];
             this.user = args[1];
 
+            // print values as confirmation
             Console.WriteLine($"Server: {this.server}");
             Console.WriteLine($"User: {this.user}");
 
+            // if the user provided more arguments
+            // copy them into the `commands` array
             if (args.Length > 2) {
                 this.commands = new string[args.Length - 2];
                 Array.Copy(args, 2, this.commands, 0, args.Length - 2);
