9962cf5a1a377edd6cb972772946350db5a6cafb
Michelle Duer <michelle.duer@gmail.com>
2019-07-13 15:59:29 -0700

    a session init class for prompting user for server, user, commands
    pull out I/O details for improved testing

---
+++ FtpClient/FtpCli/InitializeSession.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace FtpCli
+{
+  public class InitializeSession
+  {
+      Data data;
+
+      public InitializeSession() {
+        data = new Data();
+      }
+
+
+    // prompt user for server, username, and commands (if provided)
+    public Data initialPrompt(string[] args) {
+      string response = "";
+
+      // prompt for server
+      do {
+        response = this.promptUser("Enter Server: ");
+      } while (!validateNonEmptyResponse(response));
+      this.data.setServer(response);
+
+      do {
+        response = this.promptUser("Enter Username: ");
+      } while (!validateNonEmptyResponse(response));
+      this.data.setUser(response);
+
+      response = this.promptUser("Enter Command (or `Enter` if none): ");
+      string[] responseAsArray = response.Split(" ");
+      this.data.setCommands(responseAsArray);
+
+      return this.data;
+    }
+
+    // prompts user and returns input
+    public string promptUser(string prompt) {
+
+      Console.WriteLine(prompt);
+      return Console.ReadLine();
+    }
+
+    // validates that the response contains valid string content
+    public bool validateNonEmptyResponse(string response) 
+    {
+      // incorrect number of values passed in
+      if (response == "") {
+        return false;
+      }
+      return true;
+    }
+
+    public void confirmServer() {
+      Console.Write("\nServer: ");
+      this.data.printServer();
+    }
+
+    public void confirmUser() {
+      Console.Write("\nUser: ");
+      this.data.printUser();
+    }
+
+    public void confirmCommands() {
+      Console.Write("\nCommands: ");
+      this.data.printCommands();
+    }
+  }
+}
