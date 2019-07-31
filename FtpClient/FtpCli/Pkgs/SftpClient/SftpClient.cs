de9005598a3daa575219d579fd0bf9d9501b7dab
Karis Sponsler <karis.sponsler@gmail.com>
2019-07-30 19:08:42 -0700

    get file from remote

--- FtpClient/FtpCli/Pkgs/SftpClient/SftpClient.cs
+++ FtpClient/FtpCli/Pkgs/SftpClient/SftpClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Renci.SshNet;
 using Renci.SshNet.Sftp;
 
@@ -83,6 +84,18 @@ namespace FtpCli.Packages.ClientWrapper
                 }
             } catch {
                 Console.WriteLine($"Could not find directory {path}");
+            }
+        }
+
+        public void GetFile(string srcPath, string destPath)
+        {
+            try {
+                using(var file = File.OpenWrite(destPath)) {
+                    _client.DownloadFile(srcPath, file);
+                }
+                Console.WriteLine($"Wrote {srcPath} to {destPath}");
+            } catch {
+                Console.WriteLine($"Could not get file {srcPath} and write to {destPath}");
             }
         }
     }
